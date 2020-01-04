75b81038f53ce4ee5075fa0456384a0d302292d3
keisuke ohashi <kog71903@gmail.com>
2020-01-04 11:03:24 +0900

    [update]76まで完了

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+  background-color: ${props => props.alt ? 'red' : 'green'};
+  color: white;
+  font: inherit;
+  border: 1px solid blue;
+  padding: 8px;
+  cursor: pointer;
+  &:hover {
+    background-color: ${props => props.alt ? 'salmon' : 'lightgreen'};
+    color: black;
+  }
+`;
 
 
 class App extends Component {
@@ -53,17 +67,17 @@ class App extends Component {
   }
 
   render() {
-    const style = {
-      backgroundColor: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer',
-      ':hover': {
-        backgroundColor: 'lightgreen',
-        color: 'black'
-      }
-    };
+    // const style = {
+    //   backgroundColor: 'white',
+    //   font: 'inherit',
+    //   border: '1px solid blue',
+    //   padding: '8px',
+    //   cursor: 'pointer',
+    //   ':hover': {
+    //     backgroundColor: 'lightgreen',
+    //     color: 'black'
+    //   }
+    // };
 
     let persons = null;
 
@@ -80,11 +94,11 @@ class App extends Component {
           })}
         </div>
       );
-      style.backgroundColor = 'red';
-      style[':hover'] = {
-        backgroundColor: 'salmon',
-        color: 'black'
-      };
+      // style.backgroundColor = 'red';
+      // style[':hover'] = {
+      //   backgroundColor: 'salmon',
+      //   color: 'black'
+      // };
     }
 
     const classes = [];
@@ -100,9 +114,9 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p className={classes.join(' ')}>This is really working!</p>
-        <button
-          style={style}
-          onClick={this.togglePersonsHandler} >switch name</button>
+        <StyledButton
+          alt={this.state.showPersons}
+          onClick={this.togglePersonsHandler} >switch name</StyledButton>
           {this.state.showPersons && persons}
       </div>
     );
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,5 +1,19 @@
 import React from 'react';
 import './Person.css';
+import styled from 'styled-components';
+
+const StyleDiv = styled.div`
+    width: 60%;
+    margin: 16px auto;
+    border: 1px solid #EEE;
+    box-shadow: 0 2px 3px #CCC;
+    padding: 16px;
+
+    @media (min-width: 500px) {
+      width: 450px;
+    }
+  `
+;
 
 const person = ( props ) => {
   const style = {
@@ -8,11 +22,11 @@ const person = ( props ) => {
     }
   }
   return (
-    <div className='Person' style={style}>
+    <StyleDiv>
       <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
       <p>{props.children}</p>
       <input type='text' onChange={props.changed} value={props.name} />
-    </div>
+    </StyleDiv>
   )
 }
 
